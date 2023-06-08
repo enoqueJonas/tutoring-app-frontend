8501c6c794e41559c6b8d175de62011e5fe77229
anas599 <anas599@yahoo.com>
2023-06-08 23:44:18 +0300

    update app to include side-nav-bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import SideBar from './side-nav/sideBar';
+
 function App() {
-  return <h1>App component</h1>;
+  return (
+    <section className="container-fluid">
+      <div className="row">
+        <div className="col-md-3 p-0">
+          <SideBar />
+        </div>
+        <div className="col-md-9">
+          <main role="main" className="px-4">
+            <h1>App component</h1>
+          </main>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
