403ae06785642d9ae23632a59c2d703271516198
anas599 <anas599@yahoo.com>
2023-06-09 21:51:26 +0300

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import Home from './pages/Home';
-// import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
