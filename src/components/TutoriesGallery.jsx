fdf8e1e0b95ad2dc570c49d1f0d6c6f81fa75dcd
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-09 09:28:04 -0600

    Create component to display a gallery of tutories

---
+++ src/components/TutoriesGallery.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import Tutory from './Tutory';
+import './tutories.css';
+
+export default function TutoriesGallery({
+  tutories, isComputerWidth, amountScrollPages, itemsAmount, translated,
+}) {
+  return (
+    <section className="tutories-wrapper">
+      <div
+        className="tutories"
+        style={isComputerWidth ? {
+          gridTemplateColumns: `repeat(${itemsAmount}, 1fr)`,
+          width: `${amountScrollPages}00%`,
+          transform: `translateX(${translated}%)`,
+        } : {}}
+      >
+        {tutories.map((tutory) => (
+          <Tutory
+            key={tutory.id}
+            image={tutory.image}
+            subject={tutory.subject}
+            description={tutory.description}
+            tutor={tutory.tutor}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+TutoriesGallery.propTypes = {
+  tutories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      subject: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      tutor: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        socialMedia: PropTypes.shape({
+          linkedin: PropTypes.string,
+          twitter: PropTypes.string,
+          facebook: PropTypes.string,
+        }).isRequired,
+      }).isRequired,
+    }),
+  ).isRequired,
+  isComputerWidth: PropTypes.bool.isRequired,
+  translated: PropTypes.number.isRequired,
+  amountScrollPages: PropTypes.number.isRequired,
+  itemsAmount: PropTypes.number.isRequired,
+};
