c67f167c58e77e2ae2d654b3128cffbe8fea2ae3
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-09 11:54:49 -0600

    Add redux store provider to the home component when rendering it in root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Home from './pages/Home';
+// import App from './App';
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* TODO: add router provider instead of home component */}
+      <Home />
+    </Provider>
   </React.StrictMode>,
 );
