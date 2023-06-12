5b2fa7fe026ee62972b70573fee60a83cb1d61a9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-12 13:09:15 -0600

    Link each tutory with the details page

--- src/components/home/Tutory.jsx
+++ src/components/home/Tutory.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { ReactComponent as FacebookIcon } from '../../assets/facebook.svg';
 import { ReactComponent as LinkedinIcon } from '../../assets/linkedin.svg';
 import { ReactComponent as TwitterIcon } from '../../assets/twitter.svg';
@@ -7,29 +8,33 @@ export default function Tutory({
   image, subject, description,
 }) {
   return (
-    <div className="tutory all:center-text">
-      <img src={image} alt={`Tutory class about ${subject}`} />
-      <h2>{subject}</h2>
-      <div className="separator" />
-      <p>{description}</p>
-      <ul className="social-media">
-        <li>
-          <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
-            <LinkedinIcon />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/" target="_blank" rel="noreferrer">
-            <TwitterIcon />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-            <FacebookIcon />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Link to={`/classes/${subject}`}>
+      <div className="tutory all:center-text">
+        <img src={image} alt={`Tutory class about ${subject}`} />
+        <h2>
+          {subject}
+        </h2>
+        <div className="separator" />
+        <p>{description}</p>
+        <ul className="social-media">
+          <li>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+              <LinkedinIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+              <TwitterIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+              <FacebookIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Link>
   );
 }
 
