a0716f26c540afacc3aaf324903eb6d888d203e9
Uthmanbello <uthmanbello88@gmail.com>
2023-06-12 20:02:12 +0100

    Add heading to the delete class page

--- src/components/delete-class/deleteClass.jsx
+++ src/components/delete-class/deleteClass.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTutories, deleteTutory } from '../../redux/deleteClass/deleteClassSlice';
+import './deleteClass.css'
 
 
 const DeleteClass = () => {
@@ -17,6 +18,12 @@ const DeleteClass = () => {
   
     return (
       <div>
+        <div className="all:center-text mb-1">
+          <h1 className="title">Delete a class</h1>
+          <p className="subtitle">Here are all the classes available</p>
+        </div>
+        <div className="separator mb-2" />
+
         {tutories.map((classItem) => (
           <div key={classItem.id}>
             <img src={classItem.image} alt={`Tutory class about ${classItem.subject}`} />
