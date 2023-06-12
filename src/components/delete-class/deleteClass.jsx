ca24acb443005cf252b77a9d6091b9ca7113b3ad
Uthmanbello <uthmanbello88@gmail.com>
2023-06-12 21:20:59 +0100

    Make the class div contain the contents

--- src/components/delete-class/deleteClass.jsx
+++ src/components/delete-class/deleteClass.jsx
@@ -24,22 +24,15 @@ const DeleteClass = () => {
         </div>
         <div className="separator mb-2" />
 
-        {/* {tutories.map((classItem) => (
-          <div key={classItem.id}>
-            <img src={classItem.image} alt={`Tutory class about ${classItem.subject}`} />
-            <span>{classItem.subject}</span>
-            <button onClick={() => handleDeleteClass(classItem.id)}>Delete</button>
-          </div>
-        ))} */}
-
         {tutories.map((classItem) => (
           <div key={classItem.id} className="tutory-item">
-            <div className="tutory-image" style={{ backgroundImage: `url(${classItem.image})` }} />
-            <div className="tutory-content">
-              <span className="tutory-subject">{classItem.subject}</span>
-              <button className="delete-button" onClick={() => handleDeleteClass(classItem.id)}>
-                Delete
-              </button>
+            <div className="tutory-image" style={{ backgroundImage: `url(${classItem.image})` }}>
+              <div className="tutory-content">
+                <span className="tutory-subject">{classItem.subject}</span>
+                <button className="delete-button" onClick={() => handleDeleteClass(classItem.id)}>
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
         ))}
