df72150ecb3a1aad485002ed8237e9eb54db2be7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-13 17:21:17 -0600

    Fix bug when fetching a single tutory and create basic layout for it

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import findTutoryById from '../helpers/findTutoryById';
 import { fetchTutory, setTutory, setTutoryError } from '../redux/tutories/tutoriesSlice';
 
@@ -20,12 +21,65 @@ export default function Details() {
       return;
     }
     /* Request a single tutory from API if tutories aren't loaded */
-    dispatch(fetchTutory);
+    dispatch(fetchTutory(subjectId));
   }, [tutories]);
   return (
-    <div>
-      <p>{tutory.subject}</p>
+    <section className="center-container relative">
+      <img src={tutory.image} alt={`${tutory.subject} class`} />
+      <div>
+        <h2>{tutory.subject}</h2>
+        <p>{tutory.description}</p>
+        <ul>
+          <li>
+            <span>Duration: </span>
+            <span>
+              {tutory.duration}
+              min
+            </span>
+          </li>
+          <li>
+            <span>Tutor name: </span>
+            <span>
+              {tutory.tutorName}
+            </span>
+          </li>
+          <li>
+            <span>Created at: </span>
+            <span>
+              {tutory.created_at}
+            </span>
+          </li>
+        </ul>
+        <p>
+          <strong>
+            {tutory.price}
+            $
+            {' '}
+          </strong>
+          Standard price
+        </p>
+        <Link to="/home">
+          <span>Discover more classes</span>
+          <span className="material-symbols-outlined">
+            chevron_right
+          </span>
+        </Link>
+        <button type="button">
+          Reserve
+          <span className="material-symbols-outlined">
+            arrow_circle_right
+          </span>
+        </button>
+      </div>
+      <Link
+        className="arrow arrow--left arrow--left-bottom"
+        to="/home"
+      >
+        <span className="material-symbols-outlined">
+          play_arrow
+        </span>
+      </Link>
       {tutoryError !== '' && <p>{tutoryError}</p>}
-    </div>
+    </section>
   );
 }
