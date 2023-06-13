cf4340fe4c3e14344d20ece5241b685f4adc2df9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-12 19:10:41 -0600

    Create use effect to get the class tutory subject from the details page through its id

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,31 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import findTutoryById from '../helpers/findTutoryById';
+import { fetchTutory, setTutory, setTutoryError } from '../redux/tutories/tutoriesSlice';
 
 export default function Details() {
-  const { subject } = useParams();
+  const { subjectId } = useParams();
+  const dispatch = useDispatch();
+  const { tutories, tutory, tutoryError } = useSelector((store) => store.tutories);
+  useEffect(() => {
+    /* if tutories are already loaded */
+    if (tutories.length > 0) {
+      const result = findTutoryById(tutories, subjectId);
+      if (result?.subject === null) {
+        dispatch(setTutoryError(`Tutory with id: ${subjectId} couldn't be found!`));
+        return;
+      }
+      dispatch(setTutory(result));
+      return;
+    }
+    /* Request a single tutory from API if tutories aren't loaded */
+    dispatch(fetchTutory);
+  }, [tutories]);
   return (
     <div>
-      <p>{subject}</p>
+      <p>{tutory.subject}</p>
+      {tutoryError !== '' && <p>{tutoryError}</p>}
     </div>
   );
 }
