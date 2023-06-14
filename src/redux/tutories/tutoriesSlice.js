722c359ca66fff0edd1ded064fae8a53bc540011
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-14 01:13:33 -0600

    Remove tutory slice logic from tutories slice file

--- src/redux/tutories/tutoriesSlice.js
+++ src/redux/tutories/tutoriesSlice.js
@@ -5,13 +5,8 @@ const BASE_URL = 'https://tutoring-app-backend-group.onrender.com';
 
 const initialState = {
   tutories: [],
-  tutory: {},
-  status: 'idle',
-  error: '',
-  tutoryError: '',
-  translated: 0,
-  isComputerWidth: window.matchMedia('(min-width: 1024px)').matches,
-  reachedMaxScroll: false,
+  tutoriesStatus: 'idle',
+  tutoriesError: '',
 };
 
 export const fetchTutories = createAsyncThunk(
@@ -28,81 +23,27 @@ export const fetchTutories = createAsyncThunk(
   }),
 );
 
-export const fetchTutory = createAsyncThunk(
-  'tutory/get',
-  (subjectId) => new Promise((resolve, reject) => {
-    axios
-      .get(`${BASE_URL}/class_subjects/${subjectId}`)
-      .then(({ data }) => {
-        resolve(data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  }),
-);
-
 const tutoriesSlice = createSlice({
   name: 'tutories',
   initialState,
-  reducers: {
-    updateIsComputerWidth: (state, { payload }) => ({ ...state, isComputerWidth: payload }),
-    updateHasReachedMaxScrolled: (state, { payload }) => ({ ...state, reachedMaxScroll: payload }),
-    translateLeft: (state, { payload }) => {
-      const translate = state.translated + payload;
-      return { ...state, translated: translate };
-    },
-    translateRight: (state, { payload }) => {
-      const translate = state.translated - payload;
-      return { ...state, translated: translate };
-    },
-    setTutory: (state, { payload }) => ({
-      ...state, tutory: payload,
-    }),
-    setTutoryError: (state, { payload }) => ({
-      ...state, tutoryError: payload,
-    }),
-  },
+  reducers: {},
   extraReducers(builder) {
     builder
       .addCase(fetchTutories.pending, (state) => ({
         ...state,
-        status: 'loading',
+        tutoriesStatus: 'loading',
       }))
       .addCase(fetchTutories.fulfilled, (state, { payload }) => ({
         ...state,
         tutories: payload,
-        status: 'fulfilled',
+        tutoriesStatus: 'fulfilled',
       }))
       .addCase(fetchTutories.rejected, (state, { error }) => ({
         ...state,
-        status: 'rejected',
-        error: error.message,
-      }))
-      .addCase(fetchTutory.pending, (state) => ({
-        ...state,
-        status: 'loading',
-      }))
-      .addCase(fetchTutory.fulfilled, (state, { payload }) => ({
-        ...state,
-        tutory: payload,
-        status: 'fulfilled',
-      }))
-      .addCase(fetchTutory.rejected, (state, { error }) => ({
-        ...state,
-        status: 'rejected',
-        tutoryError: error.message,
+        tutoriesStatus: 'rejected',
+        tutoriesError: error.message,
       }));
   },
 });
-
-export const {
-  updateIsComputerWidth,
-  updateHasReachedMaxScrolled,
-  translateLeft,
-  translateRight,
-  setTutory,
-  setTutoryError,
-} = tutoriesSlice.actions;
 
 export default tutoriesSlice.reducer;
