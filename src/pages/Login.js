80495f22385535e469b102cf813c49ac36e76fbc
enoqueJonas <enoque.macanda@gmail.com>
2023-06-14 21:31:50 +0200

    Got rid of sidebar in the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LoginFields from '../components/LoginFields';
 
 const Login = () => (
-  <div className="login-page-wrapper mainContainer items-center flex bg-lime-500 h-screen w-screen overflow-hidden justify-center">
+  <div className="login-page-wrapper mainContainer items-center flex bg-lime-500 h-screen w-screen overflow-hidden justify-center absolute z-50">
     <LoginFields />
   </div>
 );
