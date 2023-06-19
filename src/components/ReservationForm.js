41b89364139133125fe640f4aae7e31fad792205
enoqueJonas <enoque.macanda@gmail.com>
2023-06-19 22:22:23 +0200

    FIX linters

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useCreateReservationMutation } from '../api/reservationsApi';
 import { useCurrentUserQuery } from '../api/usersData';
