0981ace02fa547e807b5188bab9c6f2ada2bea35
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-19 10:11:19 -0600

    Remove reservations component wrapper

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReservationForm from '../components/ReservationForm';
 
 const Reservation = () => (
-  <div className="registration-page-wrapper mainContainer items-center flex bg-lime-500 h-screen w-screen overflow-hidden justify-center">
-    <ReservationForm />
-  </div>
+  <ReservationForm />
 );
 
 export default Reservation;
