8304868fb1a3610e2d437824b49ad72802e99fd6
enoqueJonas <enoque.macanda@gmail.com>
2023-06-19 22:32:17 +0200

    UPDATE ProtectedRoute.js

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,21 +1,56 @@
-import React, { useContext } from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { UserContext } from './UserContext';
+import { useCurrentUserQuery } from './api/usersData';
+import { updateUser } from './redux/tutories/tutoriesSlice';
 
-const ProtectedRoute = ({ path, element }) => {
-  const { isLoggedIn } = useContext(UserContext);
+const ProtectedRoute = ({ component: Component }) => {
+  const { data: currentUser, isLoading, isError } = useCurrentUserQuery();
+  const { user } = useSelector((store) => store.tutories);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  return isLoggedIn ? (
-    <Route path={path} element={element} />
-  ) : (
-    <Navigate to="/login" replace />
+  useEffect(() => {
+    dispatch(
+      updateUser({
+        loggedIn: currentUser && currentUser.logged_in,
+        data: (currentUser && currentUser.user) || {},
+      }),
+    );
+  }, [currentUser]);
+
+  if (isLoading) {
+    return <h1 className="absolute z-50 flex justify-center items-center text-white bg-lime-500 h-screen w-screen">Loading...</h1>;
+  }
+  if (isError) {
+    return <p>Could not open page.</p>;
+  }
+
+  const isAuthenticated = user.loggedIn;
+
+  if (!isAuthenticated) {
+    const isAuthenticatedCookie = document.cookie.includes('_tutoring_key');
+    if (isAuthenticatedCookie) {
+      dispatch(updateUser({ loggedIn: true, data: currentUser.user }));
+    } else {
+      navigate('/login'); // Redirect the user to the login page
+      return null; // Render nothing while the redirection takes place
+    }
+  }
+
+  if (!isAuthenticated) {
+    navigate('/login'); // Redirect the user to the login page
+    return null; // Render nothing while the redirection takes place
+  }
+
+  return (
+    <Component />
   );
 };
 
 ProtectedRoute.propTypes = {
-  element: PropTypes.elementType.isRequired,
-  path: PropTypes.string.isRequired,
+  component: PropTypes.elementType.isRequired,
 };
 
 export default ProtectedRoute;
