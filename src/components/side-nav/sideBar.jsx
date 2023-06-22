c6fd3f022cd39ac9e9c58f3485adb04dc91a8539
Uthmanbello <uthmanbello88@gmail.com>
2023-06-22 18:40:23 +0100

    Setup Logout button

--- src/components/side-nav/sideBar.jsx
+++ src/components/side-nav/sideBar.jsx
@@ -5,11 +5,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from '../../redux/tutories/tutoriesSlice';
 
 import './sidebar.css';
 
 function SideBar() {
+  const dispatch = useDispatch();
   const [activeItem, setActiveItem] = useState('');
+  const { loggedIn } = useSelector((state) => state.tutories.user);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
   return (
     <Navbar bg="light" expand="lg" className="sidenav d-grid ">
       <Container className="nav-container nav-color p-0  h-100">
@@ -22,6 +30,11 @@ function SideBar() {
             <Link className={`nav-link-item ${activeItem === 'myReservations' ? 'active' : ''}`} to="myReservations" onClick={() => setActiveItem('myReservations')}>My Reservations</Link>
             <Link className={`nav-link-item ${activeItem === 'addClass' ? 'active' : ''}`} to="addClass" onClick={() => setActiveItem('addClass')}>Add Class</Link>
             <Link className={`nav-link-item ${activeItem === 'deleteClass' ? 'active' : ''}`} to="deleteClass" onClick={() => setActiveItem('deleteClass')}>Delete Class</Link>
+            {loggedIn && (
+              <button className="nav-link-item logout-button" onClick={handleLogout}>
+                Logout
+              </button>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
